f4e39ff7da1c8134915984eee8de9abc5bbdebc9
Tamas Domok <tamas.domok@balabit.com>
2021-06-11 09:18:22 +0200

    false_sharing.cpp: refactor to use atomics.
    Without atomic operations I could not reproduce the effect of false_sharing.

--- false_sharing.cpp
+++ false_sharing.cpp
@@ -1,57 +1,67 @@
+#include <atomic>
 #include <benchmark/benchmark.h>
 #include <iostream>
+#include <new>
 #include <thread>
 
 // https://www.youtube.com/watch?v=c-hZpChQKe0&t=1763s
-// memory alignment
 
-// cat /sys/devices/system/cpu/cpu0/cache/index0/coherency_line_size
-static constexpr auto CacheLineSize = 64;
+// https://en.cppreference.com/w/cpp/thread/hardware_destructive_interference_size
+#ifdef __cpp_lib_hardware_interference_size
+using std::hardware_constructive_interference_size;
+using std::hardware_destructive_interference_size;
+#else
+constexpr std::size_t hardware_constructive_interference_size =
+    2 * sizeof(std::max_align_t);
+constexpr std::size_t hardware_destructive_interference_size =
+    2 * sizeof(std::max_align_t);
+#endif
 
-struct Foo { // try this too: __attribute__((__packed__))
-  char c{'x'};
-  double d{0.0};
+struct alignas(hardware_constructive_interference_size) Foo {
+  std::atomic_uint64_t x{};
+  std::atomic_uint64_t y{};
 };
 
-inline void do_work(auto &foo, double x) noexcept {
+inline void do_work_x(auto &foo) noexcept {
   for (int i = 0; i < 100'000; ++i)
-    foo.d += x;
+    foo.x.fetch_add(1, std::memory_order_relaxed);
+}
+
+inline void do_work_y(auto &foo) noexcept {
+  for (int i = 0; i < 100'000; ++i)
+    foo.y.fetch_add(-1, std::memory_order_relaxed);
 }
 
 static void BM_false_sharing(benchmark::State &state) {
-  Foo foo1, foo2;
-  assert((reinterpret_cast<long>(&foo1) - reinterpret_cast<long>(&foo2)) !=
-         CacheLineSize);
+  Foo foo;
 
   for (auto _ : state) {
-    std::thread t1{[&] { do_work(foo1, 1.0); }};
-    std::thread t2{[&] { do_work(foo2, 2.0); }};
+    std::thread t1{[&] { do_work_x(foo); }};
+    std::thread t2{[&] { do_work_y(foo); }};
     t1.join();
     t2.join();
-    benchmark::DoNotOptimize(foo1.d);
-    benchmark::DoNotOptimize(foo2.d);
+    benchmark::DoNotOptimize(foo.x);
+    benchmark::DoNotOptimize(foo.y);
     benchmark::ClobberMemory();
   }
 }
 BENCHMARK(BM_false_sharing);
 
-struct alignas(CacheLineSize) Foo2 {
-  char c{'x'};
-  double d{0.0};
+struct Foo2 {
+  alignas(hardware_destructive_interference_size) std::atomic_uint64_t x{};
+  alignas(hardware_destructive_interference_size) std::atomic_uint64_t y{};
 };
 
 static void BM_false_sharing_fixed(benchmark::State &state) {
-  Foo2 foo1, foo2;
-  assert((reinterpret_cast<long>(&foo2) - reinterpret_cast<long>(&foo1)) ==
-         CacheLineSize);
+  Foo2 foo;
 
   for (auto _ : state) {
-    std::thread t1{[&] { do_work(foo1, 1.0); }};
-    std::thread t2{[&] { do_work(foo2, 2.0); }};
+    std::thread t1{[&] { do_work_x(foo); }};
+    std::thread t2{[&] { do_work_y(foo); }};
     t1.join();
     t2.join();
-    benchmark::DoNotOptimize(foo1.d);
-    benchmark::DoNotOptimize(foo2.d);
+    benchmark::DoNotOptimize(foo.x);
+    benchmark::DoNotOptimize(foo.y);
     benchmark::ClobberMemory();
   }
 }
